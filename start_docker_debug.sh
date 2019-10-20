181a822d2e8a1863da1b463acd508d4254ab82e9
Chris Johnston <chjohnston@protonmail.com>
2019-10-20 01:21:46 -0700

    remove conflicting flags from debug script

--- start_docker_debug.sh
+++ start_docker_debug.sh
@@ -2,4 +2,4 @@ echo Building image
 docker build  -t sensormonitor_image .
 
 echo Starting sensor montior
-docker run --name sensormonitor -p 8088:80 --mount dst=/app/data,src=sensormonitor_vol --restart always --rm sensormonitor_image
+docker run --name sensormonitor -p 8088:80 --mount dst=/app/data,src=sensormonitor_vol --rm sensormonitor_image
